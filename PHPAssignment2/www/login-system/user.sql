5059d8f7d181e6775d0b95a9975d6281775057a1
炎舞雪 <96097420+EnmaYuki@users.noreply.github.com>
2022-03-29 18:42:47 +0800

    reupdate

---
+++ PHPAssignment2/www/login-system/user.sql
@@ -0,0 +1,15 @@
+
+-- Drop user
+DROP USER IF EXISTS 'databasetest1'@'localhost';
+
+-- Create Database User
+CREATE USER 'databasetest1'@'localhost' IDENTIFIED BY 'databasetest1';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'databasetest1'@'localhost' REQUIRE NONE WITH GRANT OPTION 
+	MAX_QUERIES_PER_HOUR 0, 
+	MAX_CONNECTIONS_PER_HOUR 0, 
+	MAX_UPDATES_PER_HOUR 0, 
+	MAX_USER_CONNECTIONS 0;
+
+	REVOKE ALL PRIVILEGES ON *.* FROM 'databasetest1'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'databasetest1'@'localhost'; ALTER USER 'databasetest1'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
